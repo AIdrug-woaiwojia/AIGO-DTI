ccdb972e0ad42a7a8a1e4fb5eb600bb35ca78d24
AIdrug <103670546+AIdrug-woaiwojia@users.noreply.github.com>
2023-12-10 18:49:43 +0800

    Add files via upload

---
+++ use.py
@@ -0,0 +1,49 @@
+from utils import *
+from DL_ClassifierModel import *
+from args import *
+from tool import *
+
+
+def loaddata():
+    dataClass = DataClass(dataPath='data',
+                          pSeqMaxLen=1024, dSeqMaxLen=128)
+
+    # 从文件中加载数据
+    with open('data.pkl', 'rb') as file:
+        loaded_data = pickle.load(file)
+    #
+    # 从 loaded_data 字典中获取需要的数据
+    dataClass.pSeqTokenized = loaded_data['pSeqTokenized']
+    dataClass.pSeqTokenized_k = loaded_data['pSeqTokenized_k']
+    dataClass.pContFeat = loaded_data['pContFeat']
+    dataClass.gSeqTokenized = loaded_data['gSeqTokenized']
+    dataClass.pSeqLen = loaded_data['pSeqLen']
+    dataClass.dGraphFeat = loaded_data['dGraphFeat']
+    dataClass.dFinprFeat = loaded_data['dFinprFeat']
+    dataClass.dContFeat = loaded_data['dContFeat']
+    dataClass.dAdjMat = loaded_data['dAdjMat']
+    dataClass.dSeqTokenized = loaded_data['dSeqTokenized']
+    dataClass.dSeqTokenized_k = loaded_data['dSeqTokenized_k']
+    dataClass.dSeqLen = loaded_data['dSeqLen']
+    dataClass.trainSampleNum =  loaded_data['trainSampleNum']
+    dataClass.validSampleNum = loaded_data['validSampleNum']
+    dataClass.testSampleNum = loaded_data['testSampleNum']
+    dataClass.eSeqData = loaded_data['eSeqData']
+    dataClass.edgeLab = loaded_data['edgeLab']
+    dataClass.dMolData = loaded_data['dMolData']
+    dataClass.smiles = loaded_data['smiles']
+    return dataClass
+
+
+def train(args,log):
+    dataClass = loaddata()
+    model = AIGO-DTI(args=args,log=log,cSize=dataClass.pContFeat.shape[1])
+    res = model.train(dataClass, args=args,stopRounds=-1,log=log,
+            savePath='AIGO-DTI', metrics="AUC", report=["ACC", "AUC", "LOSS",'F1','Precision','AUPR'],
+            preheat=0)
+    return res
+
+if __name__ == '__main__':
+    args = set_train_argument()
+    log = set_log('train', args.log_path)
+    train(args,log)
